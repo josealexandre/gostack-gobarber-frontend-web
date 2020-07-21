746340a3698c3a5c55c3026302315d7d26f085fc
Alexandre Junior <jasj_jr@hotmail.com>
2020-07-21 18:15:42 -0300

    Validate login form

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      {/* <SignIn /> */}
-      <SignUp />
+      <SignIn />
+      {/* <SignUp /> */}
     </>
   );
 };
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -20,7 +20,9 @@ const Signup: React.FC = () => {
     try {
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
-        email: Yup.string().required('E-mail obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
         password: Yup.string().min(6, 'Mínimo de 6 caracteres'),
       });
 
